322c198323fee2c7509f0e80d00d7f95bbf9974e
capcesar23 <cesar_a_pereira@hotmail.com>
2021-07-17 05:26:50 -0300

    teste da dio

---
+++ projetos/dio/fundamentos_python/vetores_matriz.py
@@ -0,0 +1,10 @@
+nome =  "André", "Thiago", "Bruno", "Carlos", "Cassio"
+altura = (1.71, 1.78, 1.75, 1.87, 1.71)
+
+print("-------------------")
+print("     TABELA        ")
+print("-------------------")
+
+for i in nome():
+    
+    print(f"nome: {nome}")
--- projetos/dio/provas/prova2.py
+++ projetos/dio/provas/prova2.py
@@ -1,5 +1,21 @@
+
+import sys
+
+print('Digite um numero: ')
+entrada = int(sys.stdin.readline())
+n=[]
+n.append(entrada)
+for i in range(1,10):
+    n.append(n[i-1]*2)
+for a in range(10):
+    print(f'N[{a}] = {n[a]}')
+
+
+'''
 x = int(input())
 for i in range (1,x+1):
-      i = x
-      square = (i*i)
-      print("%i^2 = %i" %(i, square))
+  x = []
+  if i <= 49:
+    square = i*i
+    print("%i^2 = %i" %(i, square))
+'''
